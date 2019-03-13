fad4368b4282a96880bb162026a406702200853a
Tatsushi Kiryu <mr.mistyrain@gmail.com>
2019-03-13 18:16:52 +0900

    implement splitter, tree and grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localeJa from '@angular/common/locales/ja';
+registerLocaleData(localeJa);
+
+import { AngularSplitModule } from 'angular-split';
+import { TreeModule } from 'angular-tree-component';
+
+import { changei18n, IgxGridModule, IgxNavbarModule } from 'igniteui-angular';
+import { IgxResourceStringsJA } from 'igniteui-angular-i18n';
+changei18n(IgxResourceStringsJA);
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +23,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NoopAnimationsModule,
+
+    AngularSplitModule.forRoot(),
+    TreeModule.forRoot(),
+
+    IgxGridModule,
+    IgxNavbarModule,
+
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'ja-JP' }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
