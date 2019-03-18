e4dea4de3878a4def43b2e2adcdea8cb7180d193
Tatsushi Kiryu <mr.mistyrain@gmail.com>
2019-03-18 19:19:02 +0900

    add nav icon

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,5 +7,6 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
+  @Input() icon = 'web';
   @Input() title = 'App Template';
 }
