2a13afb7b42e5d05957a36369dd0ea81831e44d2
Tatsushi Kiryu <mr.mistyrain@gmail.com>
2019-06-13 08:34:56 +0900

    :bug: fix(paste): skip empty line

--- src/app/shared/utils/clipboard-to-json.ts
+++ src/app/shared/utils/clipboard-to-json.ts
@@ -1,7 +1,7 @@
 import { parse } from 'papaparse';
 
 export function clipboardToJson (text: string): any[] {
-  const result = parse(text, { delimiter: '\t' });
+  const result = parse(text, { delimiter: '\t', skipEmptyLines: true });
 
   if (result.errors.length) {
     console.warn('clipboard parse error: ', JSON.stringify(result.errors, null, '  '));
