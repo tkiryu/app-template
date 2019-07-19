4d5a673a05741666c613a38e84f1facc66886415
Tatsushi Kiryu <mr.mistyrain@gmail.com>
2019-07-19 09:39:54 +0900

    :globe_with_meridians: i18n(data): change props in english

--- src/app/modules/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/modules/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -19,8 +19,8 @@ export class DashboardPageComponent implements OnInit {
 
   ngOnInit() {
     this.gridQuery.data$.subscribe(data => {
-      let male = 0;
-      let female = 0;
+      let man = 0;
+      let woman = 0;
       let twenties = 0;
       let thirties = 0;
       let fourties = 0;
@@ -30,15 +30,15 @@ export class DashboardPageComponent implements OnInit {
       let AB = 0;
       let O = 0;
       for (const item of data) {
-        item['性別'] === '男' ? male++ : female++;
+        item['Gender'] === '男' ? man++ : woman++;
 
-        const age = item['年齢']
+        const age = item['Age']
         if (20 <= age && age < 30) twenties++;
         if (30 <= age && age < 40) thirties++;
         if (40 <= age && age < 50) fourties++;
         if (50 <= age && age < 60) fifties++;
 
-        const bloodType = item['血液型'];
+        const bloodType = item['BloodType'];
         if (bloodType === 'A') A++;
         if (bloodType === 'B') B++;
         if (bloodType === 'AB') AB++;
@@ -46,8 +46,8 @@ export class DashboardPageComponent implements OnInit {
       }
 
       this.genders = [
-        { gender: 'Male', count: male },
-        { gender: 'Female', count: female }
+        { gender: 'Man', count: man },
+        { gender: 'Woman', count: woman }
       ];
 
       this.ageGroups = [
