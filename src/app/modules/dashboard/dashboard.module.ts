08306bf76ff115da33f4ad4f4524ee5336eac06a
Tatsushi Kiryu <mr.mistyrain@gmail.com>
2019-07-19 13:25:43 +0900

    :bug: fix(dashboard): import ES5 modules to avoid error

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { IgxCardModule, IgxIconModule, IgxSnackbarModule } from 'igniteui-angular';
 
-import { IgxDataChartCoreModule } from 'igniteui-angular-charts/ES2015/igx-data-chart-core-module';
-import { IgxDataChartCategoryModule } from 'igniteui-angular-charts/ES2015/igx-data-chart-category-module';
-import { IgxDataChartAnnotationModule } from 'igniteui-angular-charts/ES2015/igx-data-chart-annotation-module';
-// import { IgxLegendModule } from 'igniteui-angular-charts/ES2015/igx-legend-module';
+import { IgxDataChartCoreModule } from 'igniteui-angular-charts/ES5/igx-data-chart-core-module';
+import { IgxDataChartCategoryModule } from 'igniteui-angular-charts/ES5/igx-data-chart-category-module';
+import { IgxDataChartAnnotationModule } from 'igniteui-angular-charts/ES5/igx-data-chart-annotation-module';
+// import { IgxLegendModule } from 'igniteui-angular-charts/ES5/igx-legend-module';
 
-import { IgxDoughnutChartModule } from 'igniteui-angular-charts/ES2015/igx-doughnut-chart-module';
-import { IgxPieChartModule } from 'igniteui-angular-charts/ES2015/igx-pie-chart-module';
-import { IgxItemLegendModule } from 'igniteui-angular-charts/ES2015/igx-item-legend-module';
+import { IgxDoughnutChartModule } from 'igniteui-angular-charts/ES5/igx-doughnut-chart-module';
+import { IgxPieChartModule } from 'igniteui-angular-charts/ES5/igx-pie-chart-module';
+import { IgxItemLegendModule } from 'igniteui-angular-charts/ES5/igx-item-legend-module';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
