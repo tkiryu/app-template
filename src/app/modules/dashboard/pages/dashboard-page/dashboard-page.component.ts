8d051b4f3dc6c5a2a9c0d894d104a8f8e04ae718
Tatsushi Kiryu <mr.mistyrain@gmail.com>
2019-07-19 13:40:51 +0900

    :bug: fix(dashboard): update data via BehaviorSubject

--- src/app/modules/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/modules/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+import { BehaviorSubject, Observable } from 'rxjs';
+
 import { GridQuery } from 'src/app/states/grid';
 
 @Component({
@@ -9,16 +11,28 @@ import { GridQuery } from 'src/app/states/grid';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardPageComponent implements OnInit {
-  ageGroups: any[];
-  genders: any[];
-  bloodTypes: any[];
+  private _ageGroups$ = new BehaviorSubject<any[]>([]);
+
+  get ageGroups$(): Observable<any[]> {
+    return this._ageGroups$.asObservable();
+  }
+
+  private _genders$ = new BehaviorSubject<any[]>([]);
+
+  get genders$(): Observable<any[]> {
+    return this._genders$.asObservable();
+  }
+
+  private _bloodTypes$ = new BehaviorSubject<any[]>([]);
+
+  get bloodTypes$(): Observable<any[]> {
+    return this._bloodTypes$.asObservable();
+  }
 
   constructor(private gridQuery: GridQuery) { }
 
   ngOnInit() {
     this.gridQuery.data$.subscribe(data => {
-      let man = 0;
-      let woman = 0;
       let twenties = 0;
       let thirties = 0;
       let fourties = 0;
@@ -27,9 +41,9 @@ export class DashboardPageComponent implements OnInit {
       let B = 0;
       let AB = 0;
       let O = 0;
+      let man = 0;
+      let woman = 0;
       for (const item of data) {
-        item['Gender'] === '男' ? man++ : woman++;
-
         const age = item['Age']
         if (20 <= age && age < 30) twenties++;
         if (30 <= age && age < 40) thirties++;
@@ -41,26 +55,28 @@ export class DashboardPageComponent implements OnInit {
         if (bloodType === 'B') B++;
         if (bloodType === 'AB') AB++;
         if (bloodType === 'O') O++;
+
+        item['Gender'] === '男' ? man++ : woman++;
       }
 
-      this.genders = [
-        { gender: 'Man', count: man },
-        { gender: 'Woman', count: woman }
-      ];
-
-      this.ageGroups = [
+      this._ageGroups$.next([
         { ageGroup: 'Twenties', count: twenties },
         { ageGroup: 'Thirties', count: thirties },
         { ageGroup: 'Fourties', count: fourties },
         { ageGroup: 'Fifties', count: fifties }
-      ];
+      ]);
 
-      this.bloodTypes = [
+      this._bloodTypes$.next([
         { bloodType: 'A', count: A },
         { bloodType: 'B', count: B },
         { bloodType: 'AB', count: AB },
         { bloodType: 'O', count: O }
-      ];
+      ]);
+
+      this._genders$.next([
+        { gender: 'Man', count: man },
+        { gender: 'Woman', count: woman }
+      ]);
     });
   }
 
